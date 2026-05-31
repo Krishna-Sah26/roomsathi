084486bea178054e8f69a64319b769b670bfcde0
Krishna Kumar Sah <sahkrishna2026@gmail.com>
2026-05-31 17:04:30 +0530

    Enhance API setup with request interceptor
    Added request interceptor to include authorization token.

--- clint/src/services/api.js
+++ clint/src/services/api.js
@@ -1,7 +1,17 @@
-import axios from "axios"
+import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://roomsathi-k4gr.onrender.com/api",
-})
+  withCredentials: true,
+});
 
-export default API
+API.interceptors.request.use((config) => {
+  const userInfo = localStorage.getItem("userInfo");
+  if (userInfo) {
+    const token = JSON.parse(userInfo)?.token;
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default API;
