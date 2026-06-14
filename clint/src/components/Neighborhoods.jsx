ef05aa07162d1960c3b21ccd75d4c4a26239aecc
krishnahacks26 <sahkrishna2026@gmail.com>
2026-06-14 13:23:59 +0530

    image

--- clint/src/components/Neighborhoods.jsx
+++ clint/src/components/Neighborhoods.jsx
@@ -5,14 +5,14 @@ const neighborhoods = [
     title: "Birgunj Market",
     subtitle: "The heart of commerce and convenience.",
     image:
-      "https://scontent.fktm7-1.fna.fbcdn.net/v/t39.30808-6/472721159_2188777714920366_450765007451783320_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=833d8c&_nc_ohc=kJMyaoNtI8AQ7kNvwHg_k1B&_nc_oc=AdoQg-2md4Bfmdg0Y5K0KF-d1MzS4Gckj83bv41WhSMU6sG9R7rgR9uBHsj_VNInJUpi4djn4sFJAEBRfSF4BH8f&_nc_zt=23&_nc_ht=scontent.fktm7-1.fna&_nc_gid=hkFkZFN_fMrP4k8lKCx81A&_nc_ss=7b2a8&oh=00_Af4OXkr-vUdO9mjt0PHOpKGWJYEPpfW2t3BJL0Ap5h7K7g&oe=6A1F42FB",
+      "https://nagarikmirror.com/19658/",
     span: "md:col-span-1 md:row-span-2",
   },
   {
     title: "Adarshnagar",
     subtitle: "Premium residential community.",
     image:
-      "https://scontent.fktm7-1.fna.fbcdn.net/v/t39.30808-6/488509757_1049783497183546_8705668052756281786_n.jpg?stp=dst-jpg_s720x720_tt6&_nc_cat=108&ccb=1-7&_nc_sid=833d8c&_nc_ohc=m4bt7glugvAQ7kNvwGHF1Sn&_nc_oc=Adpbu3BqbBOV4mfMvlrUGneGOHGXGVE-Mv2dWV--oMsCoY9xAzAnN-h6rhcyc5dT2aiqa_jmts0P0WVShQKF5q1w&_nc_zt=23&_nc_ht=scontent.fktm7-1.fna&_nc_gid=wEDItQOskCLqfLPxn2ksRg&_nc_ss=7b2a8&oh=00_Af6GXpaIHhtk0doAicb0qnABvU-ZfH-gh5o6-tgOpY20wQ&oe=6A1F3FB1",
+      "https://nagarikmirror.com/19658/",
     span: "md:col-span-1",
   },
   {
